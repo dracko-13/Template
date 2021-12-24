d826da96611a2b2c20f1800545e84513abb9e147
dracko-13 <silvarogeljosearmando@outlook.com>
2021-12-23 20:43:06 -0600

    Repo at 2021-12-23 20.42.58

--- application/db/db.php
+++ application/db/db.php
@@ -8,40 +8,24 @@
 
 		public function MariaDB() {
 			return new Medoo([
-				// [required]
-				'type'     => 'mysql',
-				'host'     => 'localhost',
-				'database' => 'informatica_db',
-				'username' => 'alumno',
-				'password' => 'alumno',
-
-				// [optional]
+				'type'      => 'mysql',
+				'host'      => 'localhost',
+				'database'  => 'informatica_db',
+				'username'  => 'alumno',
+				'password'  => 'alumno',
 				'charset'   => 'utf8mb4',
 				'collation' => 'utf8mb4_general_ci',
 				'port'      => 3306,
+				'prefix'    => '',
+				'logging'   => true,
 
-				// [optional] Table prefix, all table names will be prefixed as PREFIX_table.
-				'prefix' => '',
+				'error'     => PDO::ERRMODE_SILENT,
 
-				// [optional] Enable logging, it is disabled by default for better performance.
-				'logging' => true,
-
-				// [optional]
-				// Error mode
-				// Error handling strategies when error is occurred.
-				// PDO::ERRMODE_SILENT (default) | PDO::ERRMODE_WARNING | PDO::ERRMODE_EXCEPTION
-				// Read more from https://www.php.net/manual/en/pdo.error-handling.php.
-				'error' => PDO::ERRMODE_SILENT,
-
-				// [optional]
-				// The driver_option for connection.
-				// Read more from http://www.php.net/manual/en/pdo.setattribute.php.
-				'option' => [
+				'option'    => [
 					PDO::ATTR_CASE => PDO::CASE_NATURAL
 				],
 
-				// [optional] Medoo will execute those commands after connected to the database.
-				'command' => [
+				'command'   => [
 					'SET SQL_MODE=ANSI_QUOTES'
 				]
 			]);
