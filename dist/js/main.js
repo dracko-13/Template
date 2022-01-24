117ad2283d1fdcd09cde9e72c63ceba6a2c485e8
dracko-13 <silvarogeljosearmando@outlook.com>
2022-01-23 19:44:41 -0600

    Repo at 2022-01-23 19.35.26

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,9 +1,2 @@
 $(function() {
-	show_tables();
 });
-
-function show_tables() {
-	$.get('https://localhost/app/controllers/controller.php?choice=show_tables', function(response) {
-		console.table(response);
-	}, 'json')
-}
