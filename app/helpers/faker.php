a8530e8a3dbf9a33df70fbd214f69b8f6aec1022
dracko-13 <silvarogeljosearmando@outlook.com>
2022-01-27 17:11:41 -0600

    Repo at 2022-01-27 17.11.38

--- app/helpers/faker.php
+++ app/helpers/faker.php
@@ -12,7 +12,7 @@
 		private $rows = [];
 		private $data = [];
 
-		private const QUERY = 'INSERT INTO personas VALUES(null, ?, ?, ?, ?, ?, ?)';
+		private const QUERY = '';
 
 		private $faker = NULL;
 
@@ -23,37 +23,9 @@
 		}
 
 		function generateFakeData() {
-			for($i = 1; $i <= 10 ; $i++):
-				$this->data[] = [
-					'nombres'          => $this->faker->unique()->firstName,
-					'ap'               => $this->faker->unique()->lastName,
-					'am'               => $this->faker->unique()->lastName,
-					'fecha_nacimiento' => $this->faker->unique()->date(),
-					'numero_celular'   => $this->faker->unique()->tollFreePhoneNumber(),
-					'correo'           => $this->faker->unique()->freeEmail()
-				];
-			endfor;
-
-			return $this->data;
 		}
 
 		function addFakeData() {
-			try {
-				foreach( $this->generateFakeData() as $row ):
-					$stmt = $this->mariadb->open()->prepare( self::QUERY );
-					$stmt->bindParam( 1, $row['nombres'] );
-					$stmt->bindParam( 2, $row['ap'] );
-					$stmt->bindParam( 3, $row['am'] );
-					$stmt->bindParam( 4, $row['fecha_nacimiento'] );
-					$stmt->bindParam( 5, $row['numero_celular'] );
-					$stmt->bindParam( 6, $row['correo'] );
-					$stmt->execute();
-				endforeach;
-			} catch( PDOException $e ) {
-				make_log( __CLASS__, __FUNCTION__, $e->getMessage() );
-			} finally {
-				$this->mariadb->close();
-			}
 		}
 
 	}
