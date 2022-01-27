4b32abe9964a0c240d11c25a9bc1b7ed75a297c2
dracko-13 <silvarogeljosearmando@outlook.com>
2022-01-27 17:54:15 -0600

    Repo at 2022-01-27 17.50.15

--- app/constants/authentication.php
+++ app/constants/authentication.php
@@ -1,8 +1,8 @@
 <?php
 
-	defined( 'HOST' )     || define( 'HOST',     getenv( 'HOST' ) );
-	defined( 'DATABASE' ) || define( 'DATABASE', getenv( 'DB' ) );
-	defined( 'CHARSET' )  || define( 'CHARSET',  getenv( 'CHARSET' ) );
-	defined( 'PORT' )     || define( 'PORT',     getenv( 'PORT' ) );
-	defined( 'USERNAME' ) || define( 'USERNAME', getenv( 'USER_DB' ) );
+	defined( 'HOST'     ) || define( 'HOST',     getenv( 'HOST'             ) );
+	defined( 'DATABASE' ) || define( 'DATABASE', getenv( 'DB'               ) );
+	defined( 'CHARSET'  ) || define( 'CHARSET',  getenv( 'CHARSET'          ) );
+	defined( 'PORT'     ) || define( 'PORT',     getenv( 'PORT'             ) );
+	defined( 'USERNAME' ) || define( 'USERNAME', getenv( 'USER_DB'          ) );
 	defined( 'PASSWORD' ) || define( 'PASSWORD', getenv( 'PASSWORD_USER_DB' ) );
