8ab0d226907332e2948576ed01bff3c7514f8bd2
dracko-13 <silvarogeljosearmando@outlook.com>
2022-01-31 19:01:47 -0600

    Repo at 2022-01-31 19.01.17

--- index.php
+++ index.php
@@ -27,6 +27,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/jquery-toast-plugin@1.3.2/src/jquery.toast.css">
 		<link rel="stylesheet" type="text/css" href="dist/css/main.css">
 		<link rel="icon" type="image/ico" href="favicon.ico">
