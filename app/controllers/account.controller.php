29c56accfd5cecc506a874d6e4b3bc5eedec35be
dracko-13 <silvarogeljosearmando@outlook.com>
2022-02-06 18:35:22 -0600

    Repo at 2022-02-06 18.35.16

--- app/controllers/account.controller.php
+++ app/controllers/account.controller.php
@@ -8,37 +8,6 @@
 
 	switch ( $choice ):
 
-		case 'login':
-
-			$nickname   = $_POST[ 'nickname' ];
-			$secret_key = $_POST[ 'secret_key' ];
-			$csrf_token = $_POST[ 'csrf-token' ];
-
-			if( !isset( $csrf_token ) && $csrf_token != $_SESSION[ 'csrf' ][ 'token' ] ):
-				header( 'Location: /' );
-			else:
-
-				SESSION_START();
-
-				$_SESSION[ 'user_data' ] = array(
-					'nickname'   => $nickname,
-					'secret_key' => $secret_key
-				);
-
-				if( $nickname == 'admin' && $secret_key == 'admin' ):
-					header( 'Location: /home/' );
-				endif;
-
-			endif;
-
-		break;
-
-		case 'logout':
-			SESSION_START();
-			SESSION_DESTROY();
-			header( 'Location: /' );
-		break;
-
 		default:
 			header( 'Location: /' );
 		break;
--- index.php
+++ index.php
@@ -1,26 +1,3 @@
-<?php
-
-	date_default_timezone_set("America/Mexico_City");
-
-	SESSION_START();
-
-	$_SESSION[ 'csrf' ] = array(
-		'token'        => bin2hex( random_bytes( 32 ) ),
-		'token-expire' => time() + (60 * 15)
-	);
-
-	if( isset( $_SESSION[ 'user_data' ] ) ):
-		header( 'Location: /home/' );
-	endif;
-
-	if( time() >= $_SESSION[ 'csrf' ][ 'token-expire' ] ):
-		echo 'La sesión a expirado! <a href="/" target="self">Ir a inicio</a>';
-	endif;
-
-	require_once( $_SERVER[ 'DOCUMENT_ROOT' ] . '/vendor/autoload.php' );
-
-?>
-
 <!DOCTYPE html>
 <html lang="es-mx">
 	<head>
@@ -34,21 +11,6 @@
 		<title>Inicio</title>
 	</head>
 		<body>
-
-			<div class="container px-2 py-2">
-				<form action="app/controllers/account.controller.php?choice=login" method="post">
-					<input type="hidden" name="csrf-token" value="<?= $_SESSION[ 'csrf' ][ 'token' ] ?>">
-					<div class="form-floating mb-2">
-						<input type="text" class="form-control" id="nickname" name="nickname" placeholder="Nickname">
-						<label for="nickname">Nickname</label>
-					</div>
-					<div class="form-floating mb-2">
-						<input class="form-control" type="password" id="secret_key" name="secret_key" placeholder="******">
-						<label for="secret_key">******</label>
-					</div>
-					<button type="submit" class="btn btn-outline-secondary">Acceder</button>
-				</form>
-			</div>
 
 			<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
 			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
