3ede93c220a27657b90dc8d92069d8dee4551ec7
dracko-13 <silvarogeljosearmando@outlook.com>
2022-02-22 16:42:11 -0600

    Repo at 2022-02-22 16.37.54

--- app/helpers/faker.php
+++ app/helpers/faker.php
@@ -11,8 +11,14 @@
 
 		$faker->addProvider(new \Mmo\Faker\LoremSpaceProvider($faker));
 
+		$imageUrl = $faker->loremSpaceUrl(\Mmo\Faker\LoremSpaceProvider::CATEGORY_FACE);
 	}
 
 	function addFakeData() {
 		$db = new Databases();
+		try {
+			return $db->MariaDB()->select();
+		} catch(PDOException $e) {
+			make_log( 'error', __FUNCTION__ , $e->getMessage() );
+		}
 	}
