5d0c4bbfcc0760ea1da79779e77141975a10fa71
dracko-13 <silvarogeljosearmando@outlook.com>
2022-06-17 01:00:45 +0000

    Repo at 2022-06-17 01.00.41

--- jasr/index.php
+++ jasr/index.php
@@ -14,15 +14,19 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
-		<link rel="stylesheet" type="text/css" href="dist/css/main.css">
-		<link rel="icon" type="image/ico" href="favicon.ico">
+		<link rel="stylesheet" type="text/css" href="<?= $_SERVER[ 'HTTP_REFERER' ]?>dist/css/main.css">
+		<link rel="icon" type="image/ico" href="<?= $_SERVER[ 'HTTP_REFERER' ]?>favicon.ico">
 		<title>Inicio</title>
 	</head>
 		<body>
 
+			<div class="container px-2 py-2">
+				<p class="h1">Hello world!</p>
+			</div>
+
 			<script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
 			<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-			<script src="dist/js/main.js"></script>
+			<script src="<?= $_SERVER[ 'HTTP_REFERER' ]?>dist/js/main.js"></script>
 
 		</body>
 </html>
